6737c864ca11b478dd5f51652250de174eab484d
Darius Morawiec <mail@voidplus.de>
2014-02-15 23:49:53 +0100

    updated jedis v2.4.0, added initial ping check

--- src/de/voidplus/redis/Redis.java
+++ src/de/voidplus/redis/Redis.java
@@ -1,15 +1,17 @@
 package de.voidplus.redis;
 
-import java.net.URI;
-
 import processing.core.*;
+import java.net.URI;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisShardInfo;
+
 
 public class Redis extends Jedis {
 	
 	private PApplet parent;
-	public final static String VERSION = "1.0";
+	public final static String WRAPPER_VERSION = "1.1";
+	public final static String JEDIS_VERSION = "2.4.0";
+	public final static String REDIS_VERSION = "2.8.5";
 	
 	public Redis(PApplet parent, String host) {
 		super(host);
@@ -38,12 +40,17 @@ public class Redis extends Jedis {
 	
 	private void init(PApplet parent){
 		this.parent = parent;
-		this.parent.println("# Redis-Wrapper v"+this.version()+" - https://github.com/voidplus/redis-processing & https://github.com/xetorthio/jedis");
+		this.parent.println("# Redis-Wrapper v"+Redis.WRAPPER_VERSION+" - https://github.com/voidplus/redis-processing");
+		this.parent.println("# Jedis-Library v"+Redis.JEDIS_VERSION+" with Redis v"+Redis.REDIS_VERSION+" support - https://github.com/xetorthio/jedis");
 		this.parent.registerDispose(this);
-	}
-    
-	public static String version() {
-		return VERSION;
+		
+		try {
+			this.ping();
+		} catch (Exception e){
+			this.parent.println("# Error: Redis-Serer not found ... exit sketch.");
+			this.parent.println(e.getMessage());
+			this.parent.exit();
+		}
 	}
 	
 	public void dispose(){
